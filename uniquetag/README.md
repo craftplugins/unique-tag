fae34559de0a397b0533a99067de26ea7b828e9a
Josh Baker <josh@joshuabaker.com>
2016-08-18 18:44:59 +0100

    Initial commit

---
+++ uniquetag/README.md
@@ -0,0 +1,14 @@
+# Unique Tag plugin for Craft CMS
+
+Prevent duplicate tags from being saved in the same group.
+
+![Icon](resources/icon.svg)
+
+## Installation
+
+To install Unique Tag, follow these steps:
+
+1. Download & unzip the file and place the `uniquetag` directory into your `craft/plugins` directory
+2. Install plugin in the Craft Control Panel under Settings > Plugins
+
+Unique Tag works on Craft 2.4.x and Craft 2.5.x.
---
+++ uniquetag/UniqueTagPlugin.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * Unique Tag plugin for Craft CMS
+ *
+ * Prevent duplicate tags from being saved in the same group.
+ *
+ * @author    Joshua Baker
+ * @copyright Copyright (c) 2016 Joshua Baker
+ * @link      https://joshuabaker.com/
+ * @package   UniqueTag
+ * @since     0.1.0
+ */
+
+namespace Craft;
+
+class UniqueTagPlugin extends BasePlugin
+{
+    /**
+     * @return mixed
+     */
+    public function init()
+    {
+        craft()->on('tags.onBeforeSaveTag', function(Event $event) {
+            $tag = $event->params['tag'];
+            $isNewTag = $event->params['isNewTag'];
+
+            if ($isNewTag) {
+                $criteria = craft()->elements->getCriteria(ElementType::Tag);
+                $criteria->title = $tag->title;
+
+                $event->performAction = $criteria->count() == 0;
+            }
+        });
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+         return Craft::t('Unique Tag');
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescription()
+    {
+        return Craft::t('Prevent duplicate tags from being saved in the same group.');
+    }
+
+    /**
+     * @return string
+     */
+    public function getDocumentationUrl()
+    {
+        return 'https://github.com/joshuabaker/uniquetag/blob/master/README.md';
+    }
+
+    /**
+     * @return string
+     */
+    public function getReleaseFeedUrl()
+    {
+        return 'https://raw.githubusercontent.com/joshuabaker/uniquetag/master/releases.json';
+    }
+
+    /**
+     * @return string
+     */
+    public function getVersion()
+    {
+        return '0.1.0';
+    }
+
+    /**
+     * @return string
+     */
+    public function getSchemaVersion()
+    {
+        return '0.1.0';
+    }
+
+    /**
+     * @return string
+     */
+    public function getDeveloper()
+    {
+        return 'Joshua Baker';
+    }
+
+    /**
+     * @return string
+     */
+    public function getDeveloperUrl()
+    {
+        return 'https://joshuabaker.com/';
+    }
+}
